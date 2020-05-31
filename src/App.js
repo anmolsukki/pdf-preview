c0250a04e752c71f644dd491e81a5508a57c1140
anmolsukki <anmolsukki94@gmail.com>
2020-05-31 13:55:08 +0530

    Pdf Preview

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+ 
+class MyApp extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+ 
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  goToPrevPage = () => {
+    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+  }
+
+  goToNextPage = () => {
+    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+  }
+ 
+  render() {
+    const { pageNumber, numPages } = this.state;
+    return (
+      <div>
+        <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+        <div>
+          <Document file="sample.pdf" onLoadSuccess={this.onDocumentLoadSuccess} >
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyApp;
